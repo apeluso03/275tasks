92802558624058f27553d50d58a7067f785ac619
Alex <apeluso@udel.edu>
2023-03-06 13:20:51 -0500

    3 more

--- src/nested.ts
+++ src/nested.ts
@@ -108,7 +108,21 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "id,name,options,points,published\n" + questions.join(",");
+    const questionCSV = questions
+        .map(
+            (question: Question): string =>
+                question.id +
+                "," +
+                question.name +
+                "," +
+                question.options.length +
+                "," +
+                question.points +
+                "," +
+                question.published
+        )
+        .join("\n");
+    return "id,name,options,points,published\n" + questionCSV;
 }
 
 /**
