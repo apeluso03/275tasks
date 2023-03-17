e66c277f61bb24ecd72ece92096ac05c32d2cd80
Alex <apeluso@udel.edu>
2023-03-17 14:42:11 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { useState } from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,45 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holidays = "💪" | "👏" | "✅" | "🚀" | "⚔️";
+
+const getHolidayAlphabetically: Record<Holidays, Holidays> = {
+    "💪": "👏",
+    "👏": "✅",
+    "✅": "🚀",
+    "🚀": "⚔️",
+    "⚔️": "💪"
+};
+
+const getHolidayByDate: Record<Holidays, Holidays> = {
+    "🚀": "✅",
+    "✅": "👏",
+    "👏": "💪",
+    "💪": "⚔️",
+    "⚔️": "🚀"
+};
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [Holiday, setHoliday] = useState<Holidays>("💪");
+
+    function get_HolidayAlphabetically(): void {
+        const nextHoliday = getHolidayAlphabetically[Holiday];
+        setHoliday(nextHoliday);
+    }
+
+    function get_HolidayByDate(): void {
+        const nextHoliday = getHolidayByDate[Holiday];
+        setHoliday(nextHoliday);
+    }
+
+    return (
+        <div>
+            <span>Holiday: {Holiday}</span>
+            <div>
+                <Button onClick={get_HolidayAlphabetically}>
+                    Advance by alphabet
+                </Button>
+                <Button onClick={get_HolidayByDate}>Advance by year</Button>
+            </div>
+        </div>
+    );
 }
